59f68feab8a583ca4a4847118d6df9c155fdbbcb
D N SRIVATSAVA <104565424+Srivatsava17@users.noreply.github.com>
2025-02-07 17:03:03 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Region - $AWS_DEFAULT_REGION
 echo Account ID - $AWS_ACCOUNT_ID
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+# aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 if [ $? -eq 0 ]; then
   echo "Successfully logged in to ECR."
 else
