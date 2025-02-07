faa8d7067a2c095cc06b045c84ee70f253f8ae5e
D N SRIVATSAVA <104565424+Srivatsava17@users.noreply.github.com>
2025-02-07 15:21:36 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,9 @@
 mkdir -p /home/ec2-user
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+if [ $? -eq 0 ]; then
+  echo "Successfully logged in to ECR."
+else
+  echo "Failed to log in to ECR."
+  exit 1
+fi
