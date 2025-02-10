4e0efd1d607965c2f06862fffd4fe3f8a0ac2308
D N SRIVATSAVA <104565424+Srivatsava17@users.noreply.github.com>
2025-02-10 15:35:24 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,13 +4,6 @@ echo Account ID - $AWS_ACCOUNT_ID
 cat /home/ec2-user/.env
 x=$(whoami)
 echo whoami - $x
-# aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-# if [ $? -eq 0 ]; then
-#   echo "Successfully logged in to ECR."
-# else
-#   echo "Failed to log in to ECR."
-#   exit 1
-# fi
 
 # AWS CLI command to retrieve secrets
 SECRET_NAME="data_lens_secrets"
@@ -22,3 +15,13 @@ SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --re
 # Parse the JSON response
 ACCOUNT_ID=$(echo $SECRET_VALUE | jq -r '.ACCOUNT_ID')
 REGION=$(echo $SECRET_VALUE | jq -r '.REGION')
+DEFAULT_REGION=REGION
+
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+if [ $? -eq 0 ]; then
+  echo "Successfully logged in to ECR."
+else
+  echo "Failed to log in to ECR."
+  exit 1
+fi
+
