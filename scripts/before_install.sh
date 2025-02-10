5b394e9b22a448701e447120bdaa61136f25395a
D N SRIVATSAVA <104565424+Srivatsava17@users.noreply.github.com>
2025-02-10 15:31:31 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,10 +4,21 @@ echo Account ID - $AWS_ACCOUNT_ID
 cat /home/ec2-user/.env
 x=$(whoami)
 echo whoami - $x
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-if [ $? -eq 0 ]; then
-  echo "Successfully logged in to ECR."
-else
-  echo "Failed to log in to ECR."
-  exit 1
-fi
+# aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+# if [ $? -eq 0 ]; then
+#   echo "Successfully logged in to ECR."
+# else
+#   echo "Failed to log in to ECR."
+#   exit 1
+# fi
+
+# AWS CLI command to retrieve secrets
+SECRET_NAME="data_lens_secrets"
+REGION="us-west-2"
+
+# Retrieve the secret value
+SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --region $REGION --query SecretString --output text)
+
+# Parse the JSON response
+ACCOUNT_ID=$(echo $SECRET_VALUE | jq -r '.ACCOUNT_ID')
+REGION=$(echo $SECRET_VALUE | jq -r '.REGION')
